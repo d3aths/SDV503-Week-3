10e5f55df93f86af5a9bce461458cf04dbc82a0f
Celeste <celeste-quinn@live.nmit.ac.nz>
2020-03-09 14:56:00 +1300

    done heaps of shit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # SDV503-Week-3
+
+pre increment works out the increment and displays the incremented number as x
+
+post increment displays x then increments it after showing it, so we only see the result next time x is displayed
--- index.js
+++ index.js
@@ -7,3 +7,31 @@ console.log(x * y);
 console.log(x / y);
 console.log(x % y);
 console.log(x ** y);
+
+let xx = 10;
+let yy = xx;
+
+console.log ("Pre increment");
+ yy = ++xx;
+ console.log(xx);
+ console.log(yy);
+
+ console.log("Post increment");
+ 
+ yy = xx++;
+ console.log(xx);
+ console.log(yy);
+
+ for (let i=0; i <=10; i++) //i starts at 0 and increments by one while its less than 10, then stops at 10
+ console.log(i);
+
+ let z =1;
+ console.log(z);
+ console.log(10 * ++z); //this is not being changed because the value of z hasnt been included in the 10* operation, only incremented
+ console.log(z);
+ console.log(z= 10 * ++z); //this is the command that would need to be used if we want the above console log to show 20.
+
+ const firstWord = "hello ";
+ const secondWord = "world";
+ console.log(firstWord + secondWord)
+ console.log(`${firstWord} ${secondWord}`) //removes the necessity for adding variables with spaces, pluses and other strings
